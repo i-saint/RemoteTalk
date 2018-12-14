d79b26148f9ee3aefe5ed995f4fda79632b4cd8a
i-saint <saint.skr@gmail.com>
2018-12-15 05:42:40 +0900

    change directory structure

--- Plugin/deploy_debug.bat
+++ Plugin/deploy_debug.bat
@@ -1,6 +1,6 @@
-set PLUGIN32_DIR="..\RemoteTalk\Assets\IST\RemoteTalk\Plugins\x86\"
-set PLUGIN64_DIR="..\RemoteTalk\Assets\IST\RemoteTalk\Plugins\x86_64\"
-set ASSET_DIR="..\RemoteTalk\Assets\StreamingAssets\RemoteTalkAssets\"
+set PLUGIN32_DIR="..\Assets\IST\RemoteTalk\Plugins\x86\"
+set PLUGIN64_DIR="..\Assets\IST\RemoteTalk\Plugins\x86_64\"
+set ASSET_DIR="..\Assets\StreamingAssets\RemoteTalkAssets\"
 set OUT32_DIR="_out\Win32_Debug\"
 set OUT64_DIR="_out\x64_Debug\"
 
